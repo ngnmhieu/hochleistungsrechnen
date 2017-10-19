c9c05839ffd4aa577361cf0911d9f1f10ab0ce56
Hieu Nguyen <ngnmhieu@gmail.com>
2017-10-19 23:27:06 +0200

    Aufgabe 2

--- Blatt1/map/map.c
+++ Blatt1/map/map.c
@@ -1,23 +1,70 @@
 #include <stdio.h>
+#define MAP_SIZE 3
 
 // Definieren Sie ein enum cardd
-
+typedef enum {
+  N = 0b0001,
+  S = 0b0010,
+  E = 0b0100,
+  W = 0b1000
+  // D.h. NW=1001, NE=0101, SE=0110, SW=1010
+} cardd;
 
 // Definieren Sie ein 3x3-Array namens map, das Werte vom Typ cardd enthält
-
+cardd map[MAP_SIZE][MAP_SIZE];
 
 // Die Funktion set_dir soll an Position x, y den Wert dir in das Array map eintragen
 // Überprüfen Sie x und y um mögliche Arrayüberläufe zu verhindern
 // Überprüfen Sie außerdem dir auf Gültigkeit
 void set_dir (int x, int y, cardd dir)
 {
+  // Überprüft die Gültigkeit von x und y
+  if (x >= MAP_SIZE || y >= MAP_SIZE)
+    return;
 
+  // Himmelsrichtung setzen wenn dir gültig ist
+  switch((int)dir) {
+    case N:
+    case E:
+    case S:
+    case W:
+    case N|W:
+    case N|E:
+    case S|E:
+    case S|W:
+      map[x][y] = dir;
+  }
 }
 
 // Die Funktion show_map soll das Array in Form einer 3x3-Matrix ausgeben
 void show_map (void)
 {
+  for (int i = 0; i < MAP_SIZE; i++) {
+    for (int j = 0; j < MAP_SIZE; j++) {
 
+      int dir = (int)map[i][j];
+      switch(dir) {
+        case N: printf("N"); break;
+        case E: printf("E"); break;
+        case S: printf("S"); break;
+        case W: printf("W"); break;
+        case N|W: printf("NW"); break;
+        case N|E: printf("NE"); break;
+        case S|E: printf("SE"); break;
+        case S|W: printf("SW"); break;
+        default:
+          printf("0");
+      }
+      
+      // Leerzeichen ausgeben
+      if (dir == (N|W) || dir == (S|W)) {
+        printf("  ");
+      } else {
+        printf("   ");
+      }
+    }
+    printf("\n");
+  }
 }
 
 int main (void)
