50602320d9f07d65960a22a87d2f79024d8f6dcb
gid <omnesiaorg@gmail.com>
2017-11-30 21:23:44 +0100

    commented timepi.c

--- Blatt6/timempi.c
+++ Blatt6/timempi.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 #include <mpi.h>
-#include <unistd.h>
+#include <unistd.h>		// library for gethostname()
 #include <limits.h>
-#include <sys/time.h>
+#include <sys/time.h>		// library for gettimeofday()
 #define TIMESTR_MAX_LEN 300
 
 int main(int argc, char **argv)
 {
   int pid, num_procs;
 
+  /* Spawning slave processes, based on -np flag */
   MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &pid);
-  MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-  
+  MPI_Comm_rank(MPI_COMM_WORLD, &pid);		// get rank for every process
+  MPI_Comm_size(MPI_COMM_WORLD, &num_procs);	// get number of processes
+
+  /* master */
   if (pid == 0)
   {
     char timestr[TIMESTR_MAX_LEN];
     MPI_Status status;
     for (int i = 1; i < num_procs; i++) {
-      MPI_Recv(timestr, TIMESTR_MAX_LEN, MPI_CHAR, i, 0, MPI_COMM_WORLD, &status); 
+      /* receive data from process #i -> ordered */
+      MPI_Recv(timestr, TIMESTR_MAX_LEN, MPI_CHAR, i, 0, MPI_COMM_WORLD, &status);
       printf("%s\n", timestr);
     }
   }
+  /* slaves */
   else
   {
     /* get hostname */
-    char hostname[HOST_NAME_MAX];
+    char hostname[HOST_NAME_MAX];		// HOST_NAME_MAX defined with value 64 on Linux
     gethostname(hostname, HOST_NAME_MAX);
 
     /* get time */
@@ -34,8 +38,10 @@ int main(int argc, char **argv)
     gettimeofday(&tv, &tz);
 
     char timestr[TIMESTR_MAX_LEN];
+    /* write to buffer */
     sprintf(timestr, "[pid = %d] %s: %ld.%ld", pid, hostname, tv.tv_sec, tv.tv_usec);
 
+    /* send data to master */
     MPI_Send(timestr, TIMESTR_MAX_LEN, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
   }
 
@@ -44,6 +50,7 @@ int main(int argc, char **argv)
 
   printf("Rang %d beendet jetzt!\n", pid);
 
+  /* terminate processes */
   MPI_Finalize();
 
   return 0;
