8d5e01bde279e0ab3fde9ee0c9e557846fb9c864
gid <omnesiaorg@gmail.com>
2017-12-09 22:22:10 +0100

    outsourced size and added itteration var

--- Blatt7/circle.c
+++ Blatt7/circle.c
@@ -5,6 +5,17 @@
 
 #define ROOT_PID 0
 
+int getSize(int N, int num_procs, int rank, int itteration)
+{
+  // ´N / num_procs´ splits evenly; if rest exists -> spread until no rest (<=> for rank < rest)
+  // return (rank < N % num_procs) ? N / num_procs + 1 : N / num_procs;
+  // adjusted for current ittreration (<=> shift in "rank" for indexing)
+  // added itteration * num_procs because modulo with negatives does not work
+  //      printf ("adj rank %d: %d it(%d)\n", rank, (rank - itteration + ((itteration) * num_procs)) % num_procs, itteration);
+
+  return (((rank - itteration + (itteration * num_procs)) % num_procs) < N % num_procs) ? N / num_procs + 1 : N / num_procs;
+}
+
 int*
 init (int size, int rank)
 {
@@ -31,7 +42,7 @@ circle (int* buf, int rank)
   return buf;
 }
 
-void printArray(int *buf, int size, int rank, int num_procs, int N){
+void printArray(int *buf, int size, int rank, int num_procs, int N, int itteration){
   int curr_size[1];
   int curr_buf[N / num_procs + 1];	//maybe last not used
   MPI_Status curr_status;
@@ -39,14 +50,14 @@ void printArray(int *buf, int size, int rank, int num_procs, int N){
   if (rank == ROOT_PID)
   {
     // MASTER
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < getSize(N, num_procs, ROOT_PID, itteration) ; i++)
     {
       printf ("rank %d: %d\n", rank, buf[i]);
     }
     for (int j = 1; j < num_procs; j++)
     {
       /* Get size first */
-      curr_size[0] = (j < N % num_procs) ? N / num_procs + 1 : N / num_procs;
+      curr_size[0] = getSize(N, num_procs, j, itteration);
       /* Now get buf */
       //int curr_buf = malloc(sizeof(int) * curr_size[0]);
       MPI_Recv(curr_buf, curr_size[0], MPI_INT, j, 0, MPI_COMM_WORLD, &curr_status);
@@ -89,23 +100,23 @@ main (int argc, char** argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-  // ´N / num_procs´ splits evenly; if rest exists -> spread until no rest (<=> for rank < rest)
-  size = (rank < N % num_procs) ? N / num_procs + 1 : N / num_procs;
+  //size = (rank < N % num_procs) ? N / num_procs + 1 : N / num_procs;
+  size = getSize(N, num_procs, rank, 0);
   buf = init(size, rank);
 
   if (rank == ROOT_PID)
   {
     printf("\nBEFORE\n");
   }
-  printArray(buf, size, rank, num_procs, N);
+  printArray(buf, size, rank, num_procs, N, 0);
 
-  //circle(buf, rank);
+  circle(buf, rank);
 
   if (rank == ROOT_PID)
   {
     printf("\nAFTER\n");
   }
-  printArray(buf, size, rank, num_procs, N);
+  printArray(buf, size, rank, num_procs, N, 1);
 
 
   /* wartet bis alle Ausgaben erfolgt sind */
